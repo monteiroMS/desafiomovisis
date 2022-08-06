3b85bfb8c4227f80b0f4657d95370a1d59315aab
Matheus Monteiro <matheuschran@hotmail.com>
2022-08-06 18:47:35 -0300

    cria principais páginas da aplicação [lib] instala biblioteca styled-components cria arquivos de estilo iniciais para cada página

---
+++ src/pages/Cart/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import * as Styled from './styles';
+
+const Cart = () => {
+  return (
+    <Styled.P>Este é o carrinho</Styled.P>
+  );
+};
+
+export default Cart;
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import * as Styled from './styles';
+
+const Home = () => {
+  return (
+    <Styled.P>Esta é a página principal</Styled.P>
+  );
+};
+
+export default Home;
